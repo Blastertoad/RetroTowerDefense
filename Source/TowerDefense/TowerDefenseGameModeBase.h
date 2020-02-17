0c7f223d946effe65154766c251f1ab7826f85f3
Blastertoad <blastertoad@gmail.com>
2020-02-17 15:55:40 -0800

    Initial up

---
+++ Source/TowerDefense/TowerDefenseGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TowerDefenseGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOWERDEFENSE_API ATowerDefenseGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
